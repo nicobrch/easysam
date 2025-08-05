1d8ae6d88ec8fc4fc11376eb1ccbf06293e9970a
nicobrch <nicolas.brch@gmail.com>
2025-08-05 00:03:45 -0400

    🥝 Change app title and icon

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from components.navbar import create_navbar
 
 app = Dash(__name__,
            use_pages=True,
+           title="EasySam",
            external_scripts=["https://unpkg.com/@tailwindcss/browser@4"],
            external_stylesheets=[
                "https://fonts.googleapis.com/css2?family=Inter&display=swap"])
